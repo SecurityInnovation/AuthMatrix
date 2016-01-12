681dac9244e837abddf9bdaaee2aa8186aff09aa
Michael Ayzenberg <mickayz@Michaels-MBP.seattle.securityinnovation.com>
2016-01-12 15:45:01 -0800

    update license to 2016

--- AuthMatrix.py
+++ AuthMatrix.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Mick Ayzenberg - Security Innovation
+# Copyright 2016 Mick Ayzenberg - Security Innovation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
